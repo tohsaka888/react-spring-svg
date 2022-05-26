74a4dbb9029dedbf07b42319b4ee3622a5b8209d
tohsaka888 <156132264@qq.com>
2022-05-26 15:35:56 +0800

    feat: 使用完整测试数据

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,47 @@ import Canvas from "./components/Canvas";
 import Point from "./components/Point";
 import fake_data from "./mock/fake_data.json";
 import { PointsContext } from "./Context/PointsContext";
-import { Entity } from "./type";
+import { EDGE, Entity } from "./type";
 import Line from "./components/Line";
 import { LineContext } from "./Context/LineContext";
 
 function App() {
   const [model, setModel] = useState<Entity[]>([]);
   const [property, setProperty] = useState<Entity[]>([]);
+  const [assets, setAssets] = useState<Entity[]>([]);
+  const [datameta, setDatameta] = useState<Entity[]>([]);
+  const [codeInfo, setCodeInfo] = useState<Entity[]>([]);
+  const [restPoint, setRestPoint] = useState<Entity[]>([]);
+  const [terminology, setTerminology] = useState<Entity[]>([]);
+  const [assetField, setAssetField] = useState<Entity[]>([]);
+  const [edges, setEdges] = useState<EDGE[]>([]);
+
   const [points, setPoints] = useState<
     MutableRefObject<SVGForeignObjectElement>[]
   >([]);
+
   const [lines, setLines] = useState<MutableRefObject<SVGPathElement>[]>([]);
   const formatData = useCallback(() => {
-    let centerPoint: Entity[] = [];
-    let defaultPoint: Entity[] = [];
-    let datameta: Entity[] = [];
-    let codeInfo: Entity[] = [];
-    let terminology: Entity[] = [];
-    let assetField: Entity[] = [];
     fake_data.data.entities.forEach((item) => {
       if (item.label === "model") {
         setModel((model) => [...model, item]);
       } else if (item.label === "asset") {
-        centerPoint.push(item);
+        setAssets((assets) => [...assets, item]);
       } else if (item.label === "property") {
-        console.log(item);
         setProperty((property) => [...property, item]);
       } else if (item.label === "datameta") {
-        datameta.push(item);
+        setDatameta((datameta) => [...datameta, item]);
       } else if (item.label === "codeinfo") {
-        codeInfo.push(item);
+        setCodeInfo((codeInfo) => [...codeInfo, item]);
       } else if (item.label === "terminology") {
-        terminology.push(item);
+        setTerminology((terminology) => [...terminology, item]);
       } else if (item.label === "assetfield") {
-        assetField.push(item);
+        setAssetField((assetField) => [...assetField, item]);
       } else {
-        defaultPoint.push(item);
+        setRestPoint((restPoint) => [...restPoint, item]);
       }
     });
+    setEdges(fake_data.data.edges);
   }, []);
 
   useEffect(() => {
@@ -59,18 +62,9 @@ function App() {
           <LineContext.Provider value={{ lines, setLines }}>
             {points.length > 0 && (
               <>
-                <Line
-                  fromId="model3058461587483181059"
-                  toId="property3058461587483181063"
-                  name={""}
-                  d={50}
-                />
-                <Line
-                  fromId="model3058461587483181059"
-                  toId="property3058461587483181062"
-                  name={""}
-                  d={50}
-                />
+                {edges.map((item) => (
+                  <Line {...item} d={50} />
+                ))}
               </>
             )}
             {model.map((item, index) => {
@@ -81,6 +75,36 @@ function App() {
             {property.map((item, index) => {
               let x = 60 * (index + 1);
               let y = 150;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {datameta.map((item, index) => {
+              let x = 160 * (index + 1);
+              let y = 50;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {assets.map((item) => {
+              let x = 500;
+              let y = 500;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {restPoint.map((item, index) => {
+              let x = 80 * index + 1;
+              let y = 400;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {codeInfo.map((item, index) => {
+              let x = 80 * index + 1;
+              let y = 600;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {terminology.map((item, index) => {
+              let x = 80 * index + 1;
+              let y = 300;
+              return <Point {...item} x={x} y={y} key={item.id} />;
+            })}
+            {assetField.map((item, index) => {
+              let x = 80 * index + 1;
+              let y = 700;
               return <Point {...item} x={x} y={y} key={item.id} />;
             })}
           </LineContext.Provider>
