7659fc0aa22a4cd1174abbac84e04a9e28e365cb
tohsaka888 <156132264@qq.com>
2022-05-26 15:15:40 +0800

    feat: 使用primaryColor

--- src/components/Point/index.style.ts
+++ src/components/Point/index.style.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { Flex } from "../../common.style";
+import { Flex, primaryColor } from "../../common.style";
 
 export const PointContainer = styled(Flex) <{ d: number }>`
   width: ${props => props.d + 'px'};
   height: ${props => props.d + 'px'};
   border-radius: 50%;
   border: 1px solid #f9f9f9;
-  background-color: black;
+  background-color: ${primaryColor};
   color: #fff;
   user-select: none;
   font-size: 12px;
