33d28361a93ea20c45b71f3dd7c9f1048378ddbf
tohsaka888 <156132264@qq.com>
2022-05-26 14:18:18 +0800

    feat: 画线

--- src/components/Line/index.tsx
+++ src/components/Line/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { LineContext } from "../../Context/LineContext";
+import { PointsContext } from "../../Context/PointsContext";
+import { EDGE } from "../../type";
 
-function Line() {
+function Line(edge: EDGE) {
+  const { points } = useContext(PointsContext)!;
+  const startPoint = points.find((point) => point.current.id === edge.fromId);
+  const endPoint = points.find((point) => point.current.id === edge.toId);
+  const lineRef = useRef<SVGPathElement>(null!);
+  const { setLines } = useContext(LineContext)!;
+
+  useEffect(() => {
+    if (lineRef) {
+      setLines((lines) => [...lines, lineRef]);
+    }
+  }, [setLines]);
+
   return (
-    <foreignObject style={{ width: "100px", height: "100px" }}>
-      <div>Line</div>
-    </foreignObject>
+    <>
+      {startPoint?.current && endPoint?.current && (
+        <path
+          d={`M ${startPoint?.current.x.animVal.value + 25} ${
+            startPoint?.current.y.animVal.value + 25
+          } L ${endPoint?.current.x.animVal.value + 25} ${
+            endPoint?.current.y.animVal.value + 25
+          }`}
+          stroke="black"
+          strokeWidth="1"
+          id={edge.fromId + edge.toId}
+          ref={lineRef}
+        ></path>
+      )}
+    </>
   );
 }
 
