e4bed760d3b76a74d7c0368c87a1c75e4681e1cf
tohsaka888 <156132264@qq.com>
2022-05-26 15:15:28 +0800

    feat: 增加直径入参

--- src/components/Line/index.tsx
+++ src/components/Line/index.tsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useRef } from "react";
+import { primaryColor } from "../../common.style";
 import { LineContext } from "../../Context/LineContext";
 import { PointsContext } from "../../Context/PointsContext";
 import { EDGE } from "../../type";
 
-function Line(edge: EDGE) {
+function Line({ d, ...edge }: EDGE & { d: number }) {
   const { points } = useContext(PointsContext)!;
   const startPoint = points.find((point) => point.current.id === edge.fromId);
   const endPoint = points.find((point) => point.current.id === edge.toId);
@@ -20,12 +21,12 @@ function Line(edge: EDGE) {
     <>
       {startPoint?.current && endPoint?.current && (
         <path
-          d={`M ${startPoint?.current.x.animVal.value + 25} ${
-            startPoint?.current.y.animVal.value + 25
-          } L ${endPoint?.current.x.animVal.value + 25} ${
-            endPoint?.current.y.animVal.value + 25
+          d={`M ${startPoint?.current.x.animVal.value + d / 2} ${
+            startPoint?.current.y.animVal.value + d / 2
+          } L ${endPoint?.current.x.animVal.value + d / 2} ${
+            endPoint?.current.y.animVal.value + d / 2
           }`}
-          stroke="black"
+          stroke={primaryColor}
           strokeWidth="1"
           id={edge.fromId + edge.toId}
           ref={lineRef}
