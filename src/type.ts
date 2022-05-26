ae3477174b79f74fd63c232547af90bab478f674
tohsaka888 <156132264@qq.com>
2022-05-26 14:17:14 +0800

    feat: 增加类型

--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,38 @@ export type Position = {
   x: number;
   y: number;
 }
+
+export type POINT = {
+  d?: number;
+}
+
+export type Entity = {
+  id: string;
+  name: string;
+  label: string;
+  childEntities?: {
+    id: string;
+    name: string;
+    label: string;
+  }[]
+}
+
+export type EDGE = {
+  fromId: string;
+  toId: string;
+  name: string | null;
+  isDraw?: boolean;
+  showArraw?: boolean;
+}
+
+export type DATA = {
+  model: Entity[];
+  centerPoint: Entity[];
+  defaultPoint: Entity[];
+  datameta: Entity[];
+  codeInfo: Entity[];
+  terminology: Entity[];
+  property: Entity[];
+  assetField: Entity[];
+  edges: EDGE[];
+}
